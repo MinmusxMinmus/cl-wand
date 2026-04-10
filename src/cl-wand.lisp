436384015bcc692ab4ba4b50f4942dc224553a3f
MinmusxMinmus <git.1khdc@simplelogin.com>
2026-04-10 22:14:16 +0200

    Simplify internals
    - Fix wrong mode checks by now comparing symbol names.
    - Use '-p' notation instead of 'p' for predicated.
    - Remove intermediate -<?> macro with call to reduce.
    - Add support for inserting into single symbols (calling them as
      functions).

--- src/cl-wand.lisp
+++ src/cl-wand.lisp
@@ -3,36 +3,23 @@
   (:export #:-><>))
 (in-package :cl-wand)
 
-(defvar *%available-modes%* '(> >> <> <>>))
+(defvar *modes* '(">" ">>" "<>" "<>>"))
 
-(defmacro -><> (initial-form &rest forms)
-  "Like -<>, but if any of >, >>, <>, or <>> are located inside FORMS,
-then the arrow changes behavior to match the symbol."
-  `(-<?> <> ,initial-form ,@forms))
+(defun mode-p (thing)
+  (if (and (symbolp thing)
+           (member (symbol-name thing) *modes* :test #'equal))
+      t
+      nil))
 
-(defmacro -<?> (mode initial-form &rest forms)
-  "Behaves as any of ->, ->>, -<>, or -<>> depending on MODE."
-  (if (not forms)
-      initial-form
-      (let ((form (car forms)))
-        (if (member form *%available-modes%*)
-            `(-<?> ,form ,initial-form ,@(cdr forms))
-            (let ((f (macroexpand `(insert-into ,initial-form ,(car forms) ,mode))))
-              `(-<?> ,mode ,f ,@(cdr forms)))))))
+(defun mode= (a b)
+  (when (and (mode-p a)
+             (mode-p b))
+    (string= (symbol-name a) (symbol-name b))))
 
-(defmacro insert-into (thing form mode)
-  "Insert THING into FORM based on MODE."
-  (when (listp form)
-    (case mode
-      (> (insert-second thing form))
-      (>> (insert-last thing form))
-      (<> (insert-in-diamond thing form #'insert-second))
-      (<>> (insert-in-diamond thing form #'insert-last))
-      (t nil))))
-
-(defun insert-last (thing form)
-  "Inserts THING as the last element in FORM."
-  (append form (list thing)))
+(defun diamond-p (thing)
+  "Check if THING is <>"
+  (and (symbolp thing)
+       (string= "<>" (symbol-name thing))))
 
 (defun insert-second (thing form)
   "Inserts THING as the second element in FORM."
@@ -40,14 +27,41 @@ then the arrow changes behavior to match the symbol."
       (cons thing (cdr form))
       (list* (car form) thing (cdr form))))
 
+(defun insert-last (thing form)
+  "Inserts THING as the last element in FORM."
+  (append form (list thing)))
+
 (defun insert-in-diamond (thing form fallback)
   "Replaces all instances of <> in FORM's top elements with THING.  Uses
 FALLBACK if no <> are present."
-  (let ((count (count-if #'diamondp form)))
+  (let ((count (count-if #'diamond-p form)))
     (if (= count 0)
         (funcall fallback thing form)
-        (substitute-if thing #'diamondp form))))
+        (substitute-if thing #'diamond-p form))))
 
-(defun diamondp (thing)
-  "Check if THING is <>"
-  (equal thing '<>))
+(defmacro -><> (initial-form &rest forms)
+  "Like -<>, but if any of >, >>, <>, or <>> are located inside FORMS,
+then the arrow changes behavior to match the symbol."
+  (let ((mode '<>))
+    (reduce #'(lambda (a b)
+                (cond
+                  ((mode-p a)             ; Mode and something.
+                   (setq mode a)
+                   b)
+                  ((mode-p b)             ; Non-mode and mode.
+                   (setq mode b)
+                   a)
+                  ((symbolp b)            ; Non-mode and symbol.
+                   (list b a))
+                  (t                      ; Non-mode and non-symbol.
+                   (cond
+                     ((mode= mode '>)
+                      (insert-second a b))
+                     ((mode= mode '>>)
+                      (insert-last a b))
+                     ((mode= mode '<>)
+                      (insert-in-diamond a b #'insert-second))
+                     ((mode= mode '<>>)
+                      (insert-in-diamond a b #'insert-last))))))
+            forms
+            :initial-value initial-form)))
